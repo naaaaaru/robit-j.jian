c2f933032371afd107bdab1b5419808b66ecccf2
naaaaaru <naru4231@gmail.com>
2024-07-02 11:11:43 +0900

    Add files via upload

---
+++ 로빛_18기_수습단원_정지안/HW_07_01/HW_002/701 robit work-2.cpp
@@ -0,0 +1,50 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	//피보나치 수열이 a,b,c 세변수의 서로의 합으로 구현 가능하다는것에서 착안하여 n을 3으로 나누었을때 0,1,2 가 나오는
+	// 3가지 경우의 수를 생각했다.
+	int n;
+	int a = 1, b = 1, c;
+	printf("n 입력:");
+	scanf_s("%d", &n);
+	if (n < 1)
+		printf("양수를 입력해주세요");
+	else if (n == 1)
+		printf("0");
+	else
+	switch (n % 3)
+	{
+	case 0:
+		for (int i = 2; i <= n / 3; i++)
+		{
+			c = a + b;
+			a = b + c;
+			b = a + c;
+		}
+		printf("%d번째 피보나치 수열의 값 : %d",n, b);
+		break;
+	case 1:
+		for (int i = 1; i <= n / 3; i++)
+		{
+			c = a + b;
+			a = b + c;
+			b = a + c;
+		}
+		printf("%d번째 피보나치 수열의 값 : %d",n, c);
+		break;
+	case 2:
+		for (int i = 1; i <= n / 3; i++)
+		{
+			c = a + b;
+			a = b + c;
+			b = a + c;
+		}
+		printf("%d번째 피보나치 수열의 값 : %d",n, a);
+		break;
+	default :
+			printf("잘못된 숫자를 입력하였습니다.");
+			break;
+	}
+	return 0;
+}
---
+++ 로빛_18기_수습단원_정지안/HW_07_01/HW_004/701 robit work-4.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    double num1, num2, result;
+    char operation;//char 로 기호를 입력
+
+    printf("Input Arithmetic Operation\nex) 3.4 * 8.5\n    2.9 - 5.4\n    3.9 * 8.0\n    3.9 ^ 8\n\ninput : ");
+    scanf_s("%f %c %f", &num1, &operation, &num2);
+
+    switch (operation)//+,-,*,^ 네가지의 계산을 스위치를 사용해 구현
+    {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '/':
+        if (num2 != 0)
+            result = num1 / num2;
+        else
+        {
+            printf("0으로 나눌수는 없습니다.\n");//예외 제거 0으로는 나눌수 없음
+        }
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '^':
+        result = 1;
+        for (int i = 0; i < num2; i++)//for문을 사용해 제곱을 구현 num2의 숫자만큼 num1을 곱하는것을 반복
+            result *= num1;
+        break;
+    }
+
+    printf("%f %c %f = %f\n", num1, operation, num2, result);
+    return 0;
+}
